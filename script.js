083adcbf835241b6379df8e7f29e07f74a9fb82c
Andre Ariano <andreariano@yahoo.com.br>
2022-11-10 18:35:14 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function submitForm(e){
+    e.preventDefault();
+
+    phoneNumber = $("#phoneNumber").val();
+    newLocation = "https://wa.me/+55" + cleanUpPhoneNumber(phoneNumber);
+
+    $("#wa-link").attr("href", newLocation);
+    $("#wa-link").text(newLocation);
+
+    document.location.href = newLocation;
+}
+
+function cleanUpPhoneNumber(phoneNumber) {
+    return phoneNumber
+            .replaceAll(" ", "")
+            .replaceAll("-", "")
+            .replaceAll(".", "")
+            .replaceAll("+", "");
+}
